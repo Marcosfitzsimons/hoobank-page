106b0b50dba93fee48a37f8346acbcdb5fa195ab
marcosfitzsimons <marcosfitzsimons@gmail.com>
2022-11-12 21:20:22 -0300

    Add button component

---
+++ app/components/Button.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Button = () => {
+  return (
+    <a
+      href="/"
+      className="text-black font-medium text-lg rounded-lg py-4 bg-gradient-to-r my-3 from-[#b0f2f5] to-[#33BBCF] w-full max-w-md lg:w-44 text-center hover:scale-105 transition-transform"
+    >
+      Get Started
+    </a>
+  );
+};
+
+export default Button;
