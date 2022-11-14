8b8d10c520eeb7f1eeb58735f5d27a6249f1fd0f
marcosfitzsimons <marcosfitzsimons@gmail.com>
2022-11-14 10:50:41 -0300

    Remove unnecessary styles

--- app/components/IntroSection.jsx
+++ app/components/IntroSection.jsx
@@ -4,7 +4,7 @@ import Button from "./Button";
 const IntroSection = ({ title, text }) => {
   return (
     <div className="text-center flex flex-col items-center gap-6 lg:text-start lg:items-start lg:w-[50%] lg:gap-8">
-      <h2 className="text-3xl flex flex-col gap-2 font-semibold lg:text-4xl xl:text-5xl xl:leading-[60px]">
+      <h2 className="text-3xl font-semibold lg:text-4xl xl:text-5xl xl:leading-[60px]">
         {title}
       </h2>
       <p className="text-gray-400 max-w-[85%] xl:text-lg xl:max-w-[90%] xl:leading-8">
