5e64e0ddda54d466d5b5a3746a1ef6c8ebde3ed9
marcosfitzsimons <marcosfitzsimons@gmail.com>
2022-11-14 14:10:58 -0300

    Fix alignment on large screens

--- app/components/Button.jsx
+++ app/components/Button.jsx
@@ -4,7 +4,7 @@ const Button = () => {
   return (
     <a
       href="/"
-      className="text-black font-medium rounded-lg py-4 mb-4 bg-gradient-to-r from-[#b0f2f5] to-[#33BBCF] w-full  text-center transition ease-out duration-200 drop-shadow lg:text-lg md:max-w-md lg:w-44 hover:drop-shadow-[0_-5px_30px_#124b53]"
+      className="text-black font-medium rounded-lg py-4 mb-4 bg-gradient-to-r from-[#b0f2f5] to-[#33BBCF] w-full text-center transition ease-out duration-200 drop-shadow lg:mb-0 lg:text-lg md:max-w-md lg:w-44 hover:drop-shadow-[0_-5px_30px_#124b53]"
     >
       Get Started
     </a>
